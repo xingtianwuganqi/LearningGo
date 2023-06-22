2dacbd7de279824f9597eed5a7c8e801fbcc7988
jingjun <254032134@qq.com>
2023-06-22 23:27:51 +0800

    fix

--- algorithm/request/request.go
+++ algorithm/request/request.go
@@ -3,18 +3,19 @@ package request
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func Request() {
 	resp, err := http.Get("https://m.hupu.com/")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, error := ioutil.ReadAll(resp.Body)
 	if error != nil {
-		panic(error)
+		log.Fatal(err)
 	}
 	fmt.Printf("%s", body)
 }
--- algorithm/request/tquery.go
+++ algorithm/request/tquery.go
@@ -2,24 +2,65 @@ package request
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func GetQuery() {
-	resp, err := http.Get("https://bbs.hupu.com/all-gambia")
+	url := "https://bbs.hupu.com/love" // 目标网站的URL
+	// 发送HTTP GET请求
+	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if resp.StatusCode != 200 {
-		panic(resp)
-	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	doc, docErr := goquery.NewDocumentFromReader(resp.Body)
-	if docErr != nil {
-		log.Fatal(docErr)
+	// 解析HTML
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println(doc)
+
+	//// 提取所需的数据
+	//document.Find("h1").Each(func(index int, element *goquery.Selection) {
+	//	// 输出标题文本
+	//	fmt.Println(element.Text())
+	//})
+
+	//document.Find("a").Each(func(index int, element *goquery.Selection) {
+	//	// 提取链接和链接文本
+	//	link, _ := element.Attr("href")
+	//	text := element.Text()
+	//
+	//	// 输出链接和链接文本
+	//	fmt.Println(text, "=>", link)
+	//})
+	//document.Find(".post-title").Each(func(index int, element *goquery.Selection) {
+	//	fmt.Println(element.Text())
+	//})
+	document.Find("li.bbs-sl-web-post-body").Each(func(index int, element *goquery.Selection) {
+		name := element.Find(".post-title").Text()
+		// 获取属性值
+		value, ok := element.Find("a").Attr("href")
+		if ok == false {
+			log.Fatal(ok)
+		}
+		auth := element.Find(".post-auth").Text()
+		time := element.Find(".post-time").Text()
+		dic := make(map[string]string)
+		dic["name"] = name
+		dic["url"] = value
+		dic["auth"] = auth
+		dic["time"] = time
+
+		fmt.Print(dic)
+		fmt.Println("\n")
+	})
+}
+
+// 爬取详情
+func detialQuery(url string) {
+
 }
