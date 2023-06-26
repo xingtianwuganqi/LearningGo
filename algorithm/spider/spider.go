a6d5bda1c712b2b2d163b73ffc8d09bfb260aa92
景军 <rxswift@126.com>
2023-06-26 23:34:37 +0800

    Update spider.go

--- algorithm/spider/spider.go
+++ algorithm/spider/spider.go
@@ -2,16 +2,40 @@ package spider
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
 	"net/http"
 )
 
 func GetHttp() {
-	resp, error := http.Get("https://baidu.com")
+	resp, error := http.Get("https://www.wai77.com/tag/%E7%A7%80%E4%BA%BA%E7%BD%91/")
 	if error != nil {
-		fmt.Print(error)
+		log.Fatal(error)
+		return
 	}
-	fmt.Print(resp.StatusCode)
-	if resp.StatusCode == 200 {
-		fmt.Print(resp.Body)
+	fmt.Println("++++++")
+	defer resp.Body.Close()
+	document, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
+	fmt.Println("=====")
+	document.Find("#recent-content").Each(func(i int, selection *goquery.Selection) {
+		selection.Find("div[id]").Each(func(i int, selection *goquery.Selection) {
+			fmt.Println(selection)
+			idInfo, ok := selection.Attr("id")
+			if ok {
+				fmt.Println(idInfo)
+			}
+			img, ok := selection.Find("a.thumbnail-link").First().Attr("href")
+			if ok {
+				fmt.Println(img)
+			}
+		})
+		//src, ok := selection.Find("img").Attr("src")
+		//if ok {
+		//	fmt.Println(src)
+		//}
+	})
 }
