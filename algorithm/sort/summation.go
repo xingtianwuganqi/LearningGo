bf1610458593fabb027911d012469260dfba5d81
jingjun <254032134@qq.com>
2023-08-01 18:26:01 +0800

    Update summation.go

--- algorithm/sort/summation.go
+++ algorithm/sort/summation.go
@@ -19,3 +19,20 @@ func TwoSum(nums []int, target int) []int {
 	}
 	return arr
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
